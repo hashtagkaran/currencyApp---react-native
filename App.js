a159aa80fc7802b66a5ed8a3dc023afba1cb68f9
Karan SINGH <thekrsingh@gmail.com>
2020-12-02 11:03:02 +0530

    base page

--- App.js
+++ App.js
@@ -1,114 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <ScrollView backgroundColor={'#1b262c '}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.resultCon}>
+            <Text style={styles.resultValue}>12.23</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
+          <View style={styles.inputCon}>
+            <TextInput
+              keyboardType="numeric"
+              placeholder={'Enter Value'}
+              placeholderTextColor={'#c1c1c1'}
+              style={styles.inputValue}></TextInput>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     </>
   );
 };
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#1b262c',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  resultCon: {
+    height: 70,
+    marginTop: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#bbe1fa',
+    borderWidth: 2,
   },
-  body: {
-    backgroundColor: Colors.white,
+  resultValue: {fontSize: 30, color: '#fff', fontWeight: 'bold'},
+  inputCon: {
+    height: 70,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#bbe1fa',
+    borderWidth: 2,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputValue: {
+    fontSize: 30,
+    color: '#fff',
+    textAlign: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  convertButtonCon: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
-
 export default App;
